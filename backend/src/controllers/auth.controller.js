da768ab73f35bd1a5deb2df707a18d9a4ac52629
Shweta Kharade <shwetakharade@2908@gmail.com>
2026-07-08 02:04:43 +0530

    signup controller added

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,78 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import {generateToken} from "../lib/utils.js"
+
+
+export const signup = async(req, res) => {
+    
+    const {fullName, email , password, profilePic} = req.body;
+
+    try{
+
+        if(!fullName || !email || !password){
+            return res.status(400).json({message: "fill required fields"});
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({message: "password should be at least 6 characters"});
+        }
+
+        //check if email is valid : regex
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({message: "Invalid email format"});
+        }
+
+        //const user
+        const user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({message: "User already exists with this email"});
+        }
+
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+
+        const newUser = new User({
+            fullName: fullName,
+            email : email,
+            password: hashedPassword,
+            
+        });
+
+        if(newUser){
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id:newUser._id,
+                fullName: newUser.fullName,
+                email : newUser.email,
+                profilePic : newUser.profilePic
+
+            })
+        }
+
+        //todo: send a welcome email to the user
+        
+        else{
+            return res.status(400).json({message: "invalid user data"});
+        }
+
+
+
+    }catch(err){
+        console.log("Error in sign up controller: ", err);
+        res.status(500).json({message: "internal server error"});
+    }
+}
+
+export const login = (req, res) => {
+    res.send("login endpoint");
+};
+
+
+export const logout = (req, res) => {
+    res.send("logout endpoint");
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,18 +1,13 @@
 import express from "express"
+import {login, logout, signup} from "../controllers/auth.controller.js"
 const router = express.Router();
 
 
-router.get("/signup", (req, res) => {
-    res.send("sign up endpoint");
-})
+router.post("/signup", signup);
 
-router.get("/login", (req, res) => {
-    res.send("login endpoint");
-})
+router.post("/login", login);
 
-router.get("/logout", (req, res) => {
-    res.send("logout endpoint");
-})
+router.post("/logout", logout)
 
 
 export default router;
