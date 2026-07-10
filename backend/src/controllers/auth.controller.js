2877fc4e9e00f44da4eacf0227b05bc462fd0266
Shweta Kharade <shwetakharade@2908@gmail.com>
2026-07-10 15:44:03 +0530

    login & logout functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -75,10 +75,38 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("login endpoint");
+export const login = async(req, res) => {
+  const {email, password} = req.body;
+
+  try {
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({message: "Invalid credentials!"});
+    }
+
+    const isPassword = await bcrypt.compare(password, user.password);
+
+    if(!isPassword){
+      return res.status(400).json({message: "Invalid credentials!"});
+    }
+
+    generateToken(user._id , res);
+
+    res.status(200).json({
+      _id:user._id,
+      fullName : user.fullName,
+      email: user.email,
+      profilePic : user.profilePic
+    });
+
+  } catch (error) {
+    console.log("Error in login error");
+    res.status(500).json({message: "Internal Server Error"});
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("logout endpoint");
+  res.cookie("jwt", "", {maxAge:0});
+  res.status(200).json({message: "Logout successful!"});
+ 
 };
