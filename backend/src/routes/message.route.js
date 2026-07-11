fc1f9bcf2afb438585f2e69a45f5c84af42da8de
Shweta Kharade <shwetakharade@2908@gmail.com>
2026-07-11 14:47:45 +0530

    message route added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,16 @@
 import express from "express"
+import { getAllContacts, getChatPartners, getMessagesByUserId, sendMessage } from "../controllers/message.controller.js";
+import {protectRoute} from "../middlewares/auth.middleware.js";
+import {arcjetProtection} from "../middlewares/arcjet.middleware.js"
 const router = express.Router();
 
 
-router.get("/send" , (req, res) => {
-    res.send("message sent");
-})
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id",  getMessagesByUserId);
+router.post("/send/:id" , sendMessage);
 
 
 export default router;
