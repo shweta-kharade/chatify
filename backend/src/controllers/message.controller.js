df0a5c00ce3898d657ca067e43c4dd439916b939
Shweta Kharade <shwetakharade@2908@gmail.com>
2026-07-28 11:40:19 +0530

    send message functionality implemented

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -48,7 +48,7 @@ export const sendMessage = async(req, res) => {
         const {id:receiverId}  = req.params;
         const senderId = req.user._id;
 
-        if(!text || !image){
+        if(!text  && !image){
             return res.status(400).json({message:"Text or image is required"});
         }
         if(senderId.equals(receiverId)){
